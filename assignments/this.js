65673f0de2c56c5e21538e4849d7369d2d54c92e
bigtonito39 <bigtonito@hotmail.com>
2019-09-12 19:51:31 -0400

    Strech is done!

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. windows binding : Will simply show the windows output, so, if you call a variable,
-so a variable that was declare out of a function can be called to a function 
-using the this word.
+* 1. windows binding : Will simply show the windows object output.
 * 2. Implicit binding: happens when we use this. it invoke a function.
 * 
-* 4. New Binding: construcs new objects
-  4. explicit binding:happens whe .call(), .apply(); .bind()..are use on a function
+* 4. New Binding: constructs new objects
+  4. explicit binding:happens when the .call(), .apply(); ..are call to invoke a function
 *
 * write out a code example of each explanation above
 */
@@ -15,12 +13,7 @@ using the this word.
 // Principle 1
 
 // code example for Window Binding
-var favoriteteam = function() {
-    console.log(this.myteam);
-}
-
-var myteam = "mets!";
-favoriteteam();
+console.log(this)
 
 // Principle 2
 
@@ -36,7 +29,6 @@ console.log(object.func())
 
 
 // Principle 3
-object.call()
 
 // code example for New Binding
 function Students(NewGuy) {
@@ -52,16 +44,15 @@ console.log(newStudent.subScribe())
 
 // code example for Explicit Binding
 let place = {
-    place: "France";
+    place: "France",
 }
 let places = ["italy", "dominican republic", "USA"]
 
-function likeToTravel(place1, place2, place3, place4) {
+function likeToTravel(place1, place2, place3) {
 
-    return `Hi, i like to travel to ${this.place} BUT after going to ${place2}, ${place3}, and ${place4}`;
-}
+    return `Hi, i like to travel to ${this.place} BUT after going to ${place2}, and ${place3}`;
+};
 //apply: 
 console.log(likeToTravel.apply(place, places));
 //call:
 console.log(likeToTravel.call(place, places[1], places[2], places[0]));
-//
